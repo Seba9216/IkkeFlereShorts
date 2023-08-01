68b09e4f071da235b0abce669b8f305c29e5ebe3
Sebastian Veggerby Rasmussen <Sebastian.veggerby@gmail.com>
2023-08-01 13:57:41 +0200

    Add project files.

---
+++ IkkeFlereShorts/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IkkeFlereShorts.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+
+
+}
---
+++ IkkeFlereShorts/Views/MainView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IkkeFlereShorts.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+      
+
+
+    }
+}
